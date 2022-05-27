1d2eb704453f265ddb9ea9b01116df94b2b63741
Ash Monty <git@ashmonty.com>
2022-05-28 00:39:20 +0200

    feat: add basically everything

---
+++ pages/[...params].js
@@ -0,0 +1,104 @@
+import Head from "next/head";
+import Router, { useRouter } from "next/router";
+import { SupabaseAdmin } from '../lib/supabase-admin';
+
+export async function getServerSideProps({ res, query }) {
+  // increment the redirect count
+  await SupabaseAdmin.rpc('increment_page_view', { page_slug: "redirect" });
+
+  const githubPath = query.params.join("/");
+
+  // all the needed regexes
+  const regexes = {
+    title: new RegExp("(?<=<title>).*(?=</title>)", "g"),
+    description: new RegExp(
+      '(?<=<meta name="description" content=").*(?=">)',
+      "g"
+    ),
+    image: new RegExp(
+      '(?<=<meta name="twitter:image:src" content=").*(?=" /><meta name="twitter:site" )',
+      "g"
+    ),
+  };
+
+  // fetch the actual page
+  const githubResponse = await fetch(`https://github.com/${githubPath}`, {
+    method: "GET",
+  });
+
+  const githubData = await githubResponse.text();
+
+  // extrapolate the title, description, and image
+  const pageData = {
+    title: githubData.match(regexes.title)?.[0] || "",
+    description: githubData.match(regexes.description)?.[0] || "",
+    image: githubData.match(regexes.image)?.[0] || "",
+  };
+
+  return {
+    props: {
+      pageData,
+      githubPath,
+    },
+  };
+}
+
+export default function Home({ pageData, githubPath }) {
+  if (typeof window !== "undefined") {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const router = useRouter();
+
+    router.push(`https://www.youtube.com/watch?v=dQw4w9WgXcQ`);
+
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>{pageData.title}</title>
+        <meta name="description" content={pageData.description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@github" />
+        <meta name="twitter:title" content={pageData.title} />
+        <meta name="twitter:description" content={pageData.description} />
+        <meta name="twitter:image" content={pageData.image} />
+        <meta property="og:title" content={pageData.title} />
+        <meta property="og:description" content={pageData.description} />
+        <meta property="og:image" content={pageData.image} />
+        <meta property="og:image:alt" content={pageData.description} />
+        <meta
+          property="og:url"
+          content={`https://www.microsoftgithub.com/${githubPath}`}
+        />
+        <meta property="og:site_name" content="GitHub" />
+        <meta property="og:type" content="object" />
+
+        <link
+          rel="mask-icon"
+          href="https://github.githubassets.com/pinned-octocat.svg"
+          color="#000000"
+        />
+        <link
+          rel="alternate icon"
+          className="js-site-favicon"
+          type="image/png"
+          href="https://github.githubassets.com/favicons/favicon.png"
+        />
+        <link
+          rel="icon"
+          className="js-site-favicon"
+          type="image/svg+xml"
+          href="https://github.githubassets.com/favicons/favicon.svg"
+        />
+
+        <meta name="theme-color" content="#1e2327" />
+        <meta name="color-scheme" content="dark light" />
+      </Head>
+      <p
+      >
+        Redirecting to the repository... if this doesn't work, click{" "}
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">here</a>.
+      </p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,98 @@
-export async function getServerSideProps() {
+import Head from "next/head";
+
+import { SupabaseAdmin } from "../lib/supabase-admin";
+
+import styles from "../styles/Home.module.css";
+
+export async function getServerSideProps({ res, query }) {
+  const {
+    data: {
+      [0]: { view_count },
+    },
+  } = await SupabaseAdmin.from("pages")
+    .select("view_count")
+    .filter("slug", "eq", "redirect");
+
   return {
-    /*
-     * temporary redirect, will actually work on the whole ordeal later. 
-     * also, if you're reading this: hi, welcome to the first commit! Or, well, second really, but you get what I mean.
-     */
-    redirect: {
-      destination: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", 
-      permanent: false,
+    props: {
+      rickrolled: view_count,
     },
   };
 }
 
-export default function Home() {
-  return null;
+export default function Home({ rickrolled }) {
+  return (
+    <div className={styles.wrapper}>
+      <Head>
+        <title>NotHub</title>
+        <meta
+          name="description"
+          content="This isn't GitHub, and isn't affiliated with Microsoft in any way."
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@github" />
+        <meta name="twitter:title" content="Home" />
+        <meta name="twitter:description" content="Home page" />
+      </Head>
+
+      <div className={styles.content}>
+        <div className={styles.hero}>
+          <h1>NotHub</h1>
+          <p>
+            A website that lets you create legit-looking GitHub links that
+            rickroll the visitor.
+          </p>
+        </div>
+
+        <div className={styles.section}>
+          <h1>Usage</h1>
+          <p>
+            Just replace <code className="bg">github.com</code> with{" "}
+            <code className="bg">microsoftgithub.com</code>. Yeah, that's all!
+            The resulting url will redirect to the rickroll link. For example,{" "}
+            <code>github.com/ashmonty/website</code> becomes{" "}
+            <code>microsoftgithub.com/ashmonty/website</code>.
+            <br />
+            When embedded on Twitter, Discord and other social sites, the link
+            will display the official social preview, for extra realism.
+          </p>
+        </div>
+
+        <div className={styles.section}>
+          <h1>Why?</h1>
+          <p>
+            When I found this domain I decided to buy it because I didn't want
+            it to end up in the hands of a scammer to be used as a phishing
+            link, and it was dirt cheap. With the domain in my hands, I figured
+            it would've been a shame not to use it to rickroll people.
+            <br />
+            <i>How many people, you ask?</i>{" "}
+            <code className="bg">{rickrolled}</code> and counting!
+          </p>
+        </div>
+      </div>
+
+      <footer>
+        <p>
+          Made with{" "}
+          <span role="img" aria-label="heart">
+            ❤️
+          </span>{" "}
+          by{" "}
+          <a href="https://www.ashmonty.com" target="_blank" rel="noreferrer">
+            ash
+          </a>{" "}
+          • This website is in no way affiliated with Microsoft or any of its
+          subsidiaries •{" "}
+          <a
+            href="https://github.com/ashmonty/microsoftgithub.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Source code
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
