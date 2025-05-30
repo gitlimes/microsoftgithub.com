3011a11510131562063a08ce8e2087f3ce670538
limes <git@limes.pink>
2025-05-30 07:51:36 +0200

    feat: update with code from nintenclo.com
    could probably be merged into one

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # microsoftgithub.com, a.k.a. NotHub
 
-A website that lets you create official-looking GitHub links that rickroll the visitor.
+A website that lets you create official-looking GitHub links that rickroll the visitor. For more info (and statistics), visit [microsoftgithub.com/usage](https://microsoftgithub.com/usage).
+
+Make sure to check out my sibling project, [nintenclo.com](https://nintenclo.com/usage)
 
 ## Usage
 
-Simply replace `github.com` with `microsoftgithub.com`. The resulting URL will redirect to the rickroll video, and its social media preview will look identical to the official one.
-
-
-For more info (and statistics), visit [microsoftgithub.com/usage](https://microsoftgithub.com/usage)
+Simply replace `github.com` with `microsoftgithub.com`. The resulting URL will redirect to the rickroll video, but its social media preview will look identical to the official page.
--- pages/[[...params]].js
+++ pages/[[...params]].js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import { SupabaseAdmin } from "../lib/supabase-admin";
 import { detectRobot } from "../lib/detectRobot";
+import { parse } from 'node-html-parser';
+import { decode } from "he";
 
 export async function getServerSideProps({ req, res, query }) {
-  const isGist = req.headers.host.includes("gist");
 
+  // stats (literally ONLY tracks the number of page views)
   try {
     if (detectRobot(req.headers["user-agent"])) {
       // increment the redirect count for crawlers
@@ -21,60 +23,63 @@ export async function getServerSideProps({ req, res, query }) {
     console.log("looks like supabase died.", e);
   }
 
-  const githubPath = query.params?.join("/") || "";
 
-  const actualUrl = `https://${
-    isGist ? "gist.github.com" : "github.com"
-  }/${githubPath}`;
-
-  // we check if the user has been rickrolled on this page before
-  const rickrolled = Boolean(req.cookies?.[actualUrl]);
-  // if not, we set a cookie
-  if (!rickrolled) {
-    res.setHeader("Set-Cookie", `${actualUrl}=1; path=/; Max-Age=300`);
+  let baseUrl = "";
+  if (req.headers.host.includes("microsoftgithub.com")) {
+    baseUrl = req.headers.host.replace("microsoftgithub.com", "github.com");
+  } else {
+    baseUrl = "microsoftgithub.com"; // local testing without setting up the host
   }
 
-  // all the needed regexes
-  const regexes = {
-    title: new RegExp("(?<=<title>).+?(?=</title>)", "g"),
-    twitterTitle: new RegExp(
-      '(?<=<meta name="twitter:title" content=").+?(?=")',
-      "g"
-    ),
-    description: new RegExp(
-      '(?<=<meta (name)?(property)?="description" content=").+?(?=" */*>)',
-      "g"
-    ),
-    image: new RegExp(
-      '(?<=<meta (name)?(property)?="twitter:image(:src)?" content=").+?(?=" *\/*>)',
-      "g"
-    ),
-    twitterCard: new RegExp(
-      '(?<=<meta (name)?(property)?="twitter:card" content=").+?(?=" */*>)',
-      "g"
-    ),
-  };
+  const path = query.params?.join("/") || "";
+
+  const actualUrl = `https://${baseUrl}/${path}`;
+
+  // we check if the user has been rickrolled on this page before
+  const rickrolled = Boolean(req.cookies?.[`${encodeURIComponent(`${baseUrl}/${path}`)}`]);
+  // if not, we set a cookie
+  if (!rickrolled) {
+    res.setHeader("Set-Cookie", `${encodeURIComponent(`${baseUrl}/${path}`)}==1; path=/; Max-Age=300`);
+  }
+
 
   // fetch the actual page
-  const githubResponse = await fetch(actualUrl, {
+  const fetchActualPage = await fetch(actualUrl, {
     method: "GET",
   });
 
-  const githubData = await githubResponse.text();
+  const actualPage = await fetchActualPage.text();
+
+  const actualPageDom = parse(actualPage);
 
   // extrapolate the title, description, and image
   const pageData = {
     title:
-      githubData.match(regexes.twitterTitle)?.[0] ||
-      githubData.match(regexes.title)?.[0] ||
-      "",
-    description: githubData.match(regexes.description)?.[0] || "",
-    image: githubData.match(regexes.image)?.[0] || "",
+      decode(actualPageDom.querySelector("title").innerHTML) || "",
+
+    description: actualPageDom.querySelector("meta[name='description']")?.getAttribute("content") ||
+      actualPageDom.querySelector("meta[property='description']")?.getAttribute("content") || "",
+
+    image: actualPageDom.querySelector("meta[property='twitter:image']")?.getAttribute("content") ||
+      actualPageDom.querySelector("meta[name='twitter:image']")?.getAttribute("content") ||
+      actualPageDom.querySelector("meta[property='og:image']")?.getAttribute("content") ||
+      actualPageDom.querySelector("meta[name='og:image']")?.getAttribute("content") || "",
+
     twitterCard:
-      githubData.match(regexes.twitterCard)?.[0] || "summary_large_image",
+      actualPageDom.querySelector("meta[property='twitter:card']")?.getAttribute("content") ||
+      actualPageDom.querySelector("meta[name='twitter:card']")?.getAttribute("content") || "",
+
+    twitterSite: actualPageDom.querySelector("meta[property='twitter:site']")?.getAttribute("content") ||
+      actualPageDom.querySelector("meta[name='twitter:site']")?.getAttribute("content") || "",
+
+    ogSiteName: actualPageDom.querySelector("meta[property='og:site_name']")?.getAttribute("content") ||
+    actualPageDom.querySelector("meta[property='og:site_name']")?.getAttribute("content") || "",
+
+    favicon: actualPageDom.querySelector("link[rel='icon']")?.getAttribute("href") ||
+      actualPageDom.querySelector("link[rel='shortcut icon']")?.getAttribute("href") || ""
   };
 
-  pageData.meta = Boolean(pageData.description + pageData.image); // if there's a description or image, we'll render the meta tags
+  console.log(pageData)
 
   // if the user has already been rickrolled by the page, we redirect to the actual repo
   const redirectUrl = rickrolled
@@ -84,13 +89,13 @@ export async function getServerSideProps({ req, res, query }) {
   return {
     props: {
       pageData,
-      githubPath,
+      path,
       redirectUrl,
     },
   };
 }
 
-export default function Home({ pageData, githubPath, redirectUrl }) {
+export default function Home({ pageData, path, redirectUrl }) {
   if (typeof window !== "undefined") {
     location.assign(redirectUrl);
   }
@@ -100,50 +105,32 @@ export default function Home({ pageData, githubPath, redirectUrl }) {
       <Head>
         <title>{pageData.title}</title>
 
-        {pageData.meta && (
-          <>
-            <meta name="description" content={pageData.description} />
-            <meta name="twitter:card" content={pageData.twitterCard} />
-            <meta name="twitter:site" content="@github" />
-            <meta name="twitter:title" content={pageData.title} />
-            <meta name="twitter:description" content={pageData.description} />
-            <meta name="twitter:image" content={pageData.image} />
-            <meta property="og:title" content={pageData.title} />
-            <meta property="og:description" content={pageData.description} />
-            <meta property="og:image" content={pageData.image} />
-            <meta property="og:image:alt" content={pageData.description} />
-            <meta
-              property="og:url"
-              content={`https://microsoftgithub.com/${githubPath}`}
-            />
-            <meta property="og:site_name" content="GitHub" />
-            <meta property="og:type" content="object" />
+        <meta name="description" content={pageData.description} />
+        <meta property="twitter:card" content={pageData.twitterCard} />
+        <meta property="twitter:site" content={pageData.twitterSite} />
+        <meta property="twitter:title" content={pageData.title} />
+        <meta property="twitter:description" content={pageData.description} />
+        <meta property="twitter:image" content={pageData.image} />
+        <meta property="og:title" content={pageData.title} />
+        <meta property="og:description" content={pageData.description} />
+        <meta property="og:image" content={pageData.image} />
+        <meta property="og:image:alt" content={pageData.description} />
+        <meta
+          property="og:url"
+          content={`https://microsoftgithub.com/${path}`}
+        />
+        <meta property="og:site_name" content={pageData.ogSiteName} />
+        <meta property="og:type" content="object" />
 
-            <link
-              rel="mask-icon"
-              href="https://github.githubassets.com/pinned-octocat.svg"
-              color="#000000"
-            />
-            <link
-              rel="alternate icon"
-              className="js-site-favicon"
-              type="image/png"
-              href="https://github.githubassets.com/favicons/favicon.png"
-            />
-            <link
-              rel="icon"
-              className="js-site-favicon"
-              type="image/svg+xml"
-              href="https://github.githubassets.com/favicons/favicon.svg"
-            />
-
-            <meta name="theme-color" content="#1e2327" />
-          </>
-        )}
+        <link
+          rel="icon"
+          href={pageData.favicon}
+        />
       </Head>
+      
+      {/**
       <p style={{ margin: "1em", fontSize: "18px" }}>
-        Redirecting to the repository... if you aren't being redirected
-        automatically, click{" "}
+        We're taking you to your requested page. If you're not redirected automatically, just click {" "}
         <a
           style={{ fontWeight: 500, textDecoration: "underline" }}
           href={redirectUrl}
@@ -158,6 +145,7 @@ export default function Home({ pageData, githubPath, redirectUrl }) {
         aria-hidden="true"
         href="https://fedi.limes.pink/@limes"
       ></a>
+      **/}
     </div>
   );
 }
